d0f7c5acec65e4c12071d5fa4838d0af48ec2c75
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-06 23:54:28 -0600

    Entidades

---
+++ Parcial1/Parcial1/Entidades/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1.Entidades
+{
+    public class Venta
+    {
+        public int IdentificadorDeProducto { get; set; }
+        public string? NombreDelProducto { get; set; }
+        public string? DescripcionDelProducto { get; set; }
+        public double PrecioDelProducto { get; set; }
+        public int CantidadDeProducto { get; set; }
+        public int Descontar { get; set; }
+    }
+}
