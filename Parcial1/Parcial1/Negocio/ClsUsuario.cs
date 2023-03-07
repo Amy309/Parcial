81075d3a9e6a942f68d9f36348fa42c0de21a299
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-06 23:55:17 -0600

    Funciones

---
+++ Parcial1/Parcial1/Negocio/ClsUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parcial1.Entidades;
+
+namespace Parcial1.Negocio
+{
+    public class ClsUsuario
+    {
+        public bool Autenticacion(Usuario user)
+        {
+
+            if (user.user.Equals("TUNOMBRE") && user.password.Equals("123"))
+            {
+
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+}
---
+++ Parcial1/Parcial1/Negocio/ClsVenta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parcial1.Entidades;
+
+namespace Parcial1.Negocio
+{
+    public class ClsVenta
+    {
+        public String Cobro(Venta venta)
+        {
+            double Descuento = venta.PrecioDelProducto * venta.CantidadDeProducto;
+
+            if (Descuento > 50)
+            {
+                venta.Descontar = (int)(Descuento - 10);
+                return ($"Su total a pagar con descuento es {venta.Descontar}");
+            }
+            else
+            {
+                return ($"Total a pagar {Descuento}");
+            }
+        }
+    }
+}
